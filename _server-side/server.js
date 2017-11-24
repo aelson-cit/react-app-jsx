b3621d0e29818a29c6019f1b5341d951ce4d1cdd
andre <asartori@ciandt.com>
2017-11-24 19:31:04 -0200

    server side react render

--- _server-side/server.js
+++ _server-side/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 
-app.set('views', __dirname + '/components');
+app.set('views', '_components');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
